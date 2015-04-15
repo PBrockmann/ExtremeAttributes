f420d25c131edd1ae53aa5e603ff97aee634f016
Cathy Nangini <cnangini@gmail.com>
2015-04-15 16:00:30 +0200

    Displays number of Event Type attributes selected

--- display_fns.js
+++ display_fns.js
@@ -44,6 +44,7 @@ function initCrossfilter() {
     .margins({top: 10, right: 10, bottom: 30, left: 10})    // Default margins: {top: 10, right: 50, bottom: 30, left: 30}.
     .dimension(eventDimension)
     .group(eventGrouping)
+    .on("preRedraw",update0)
     .colors(d3.scale.category20()) 
     .elasticX(true)
     .gap(0)
@@ -51,6 +52,35 @@ function initCrossfilter() {
 
   dc.renderAll();
 
+}
+
+// Update map markers, list and number of selected
+function update0() {
+  // updateMarkers();
+  // updateList();
+  d3.select("#active").text(filter.groupAll().value());
+}
+
+// Update dc charts, map markers, list and number of selected
+function update1() {
+  dc.redrawAll();
+  //updateMarkers();
+  //updateList();
+  d3.select("#active").text(filter.groupAll().value()); //Renders number of proxies selected
+  // levelZoom = map.getZoom();
+  // switch(true) {
+  // case (levelZoom > 5): 
+  //   grat_01.setStyle({opacity: 1.});
+  //   break;
+  // case (levelZoom > 3): 
+  //   grat_01.setStyle({opacity: 0.});
+  //   grat_05.setStyle({opacity: 1.});
+  //   break;
+  // default : 
+  //   grat_01.setStyle({opacity: 0.});
+  //   grat_05.setStyle({opacity: 0.});
+  //   break;
+  //}
 }
 
 function eventList() {
@@ -131,6 +161,6 @@ function eventList() {
                 .style("text-align", "right")
                 .attr("title", points[i].Reference)
                 .text(points[i].Statistic);
-        }  
+        }
+}
 
-      }
